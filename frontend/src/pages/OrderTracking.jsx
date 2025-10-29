e409ffb6f566dbb23ac6a871bace4ec286a39e3b
Saqib Arshad <saqibarshad429@gmail.com>
2025-10-29 23:47:30 +0500

    Routing problem fixed

--- frontend/src/pages/OrderTracking.jsx
+++ frontend/src/pages/OrderTracking.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { 
   Box, 
   Container, 
